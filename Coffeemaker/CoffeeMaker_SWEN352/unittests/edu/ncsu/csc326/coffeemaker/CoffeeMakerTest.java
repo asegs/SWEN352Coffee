ee71ec7501f40db2519281a7b6b80476f98d11c6
ajs2290 <ajs2290@g.rit.edu>
2021-02-10 18:59:52 -0500

    Fixed sugar bug, recipes now delete to null, and name is inserted when editing.  10 tests

--- Coffeemaker/CoffeeMaker_SWEN352/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ Coffeemaker/CoffeeMaker_SWEN352/unittests/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -78,4 +78,63 @@ public class CoffeeMakerTest extends TestCase{
 		
 	}
 	
+	@Test
+	public void rightRecipeReplaced() {
+		CoffeeMaker CuT = new CoffeeMaker();
+		Recipe r1 = new Recipe();
+		r1.setName("R1");
+		CuT.addRecipe(r1);
+		Recipe r2 = new Recipe();
+		r2.setName("R2");
+		assertEquals("R1",CuT.editRecipe(0, r2));
+	}
+	
+	@Test
+	public void newRecipeCorrect() {
+		CoffeeMaker CuT = new CoffeeMaker();
+		Recipe r1 = new Recipe();
+		r1.setName("R1");
+		CuT.addRecipe(r1);
+		Recipe r2 = new Recipe();
+		r2.setName("R2");
+		CuT.editRecipe(0, r2);
+		assertEquals("R2",CuT.getRecipes()[0].getName());
+	}
+	
+	@Test
+	public void testNoRecipe() {
+		CoffeeMaker CuT = new CoffeeMaker();
+		Recipe r1 = new Recipe();
+		r1.setName("R1");
+		assertEquals(null,CuT.editRecipe(0, r1));
+	}
+	
+	@Test
+	public void addInventoryNormal() throws InventoryException {
+		CoffeeMaker CuT = new CoffeeMaker();
+		CuT.addInventory("15", "1", "1000", "1");
+	}
+	
+	@Test
+	public void addInventoryNegative() {
+		CoffeeMaker CuT = new CoffeeMaker();
+		try {
+			CuT.addInventory("15", "-1", "1000", "1");
+			assertTrue(false);
+		} catch (InventoryException e) {
+			;
+		}
+	}
+	
+	@Test
+	public void addInventoryText() {
+		CoffeeMaker CuT = new CoffeeMaker();
+		try {
+			CuT.addInventory("a", "1", "1000", "1");
+			assertTrue(false);
+		} catch (InventoryException e) {
+			;
+		}
+	}
+	
 }
